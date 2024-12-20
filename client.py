d7f94bbf83c69bda84b85e14794d77ffdc6f2e1f
Tran Quoc Vy <quocvy23072005@gmail.com>
2024-12-20 23:35:16 +0700

    progress bar

--- client.py
+++ client.py
@@ -15,12 +15,21 @@ def upload_file(client_socket, filepath):
     if os.path.isfile(filepath):
         client_socket.send(f"upload {filename}".encode('utf-8'))
         client_socket.recv(1024)
-
+        #Tính toán kích thước file
+        total_size = os.path.getsize(filepath)
+        byte_sent = 0
+        #Upload
         with open(filepath, 'rb') as f:
             while (data := f.read(1024)):
                 client_socket.send(data)
                 client_socket.recv(1024)
+                #Cập nhật byte đã gửi
+                byte_sent += len(data)
+                #Tính toán phần trăm và hiển thị tiến độ
+                progress = (byte_sent/total_size)*100
+                print(f"\rUploading: {progress:.2f}% completed", end="")
         client_socket.send(b'END')
+        print("\nUpload completed!")
         print(client_socket.recv(1024).decode('utf-8'))
         
     elif os.path.isdir(filepath):
@@ -35,14 +44,22 @@ def upload_file(client_socket, filepath):
                 # Gửi thông tin file
                 client_socket.send(f"file {relative_path}".encode('utf-8'))
                 ack = client_socket.recv(1024)
-     
+                #Tính toán kích thước file
+                total_size = os.path.getsize(file_path)
+                byte_sent = 0
                 # Gửi nội dung file
                 with open(file_path, 'rb') as f:
                     while (data := f.read(1024)):
                         client_socket.send(data)
                         ack = client_socket.recv(1024)
+                        #Cập nhật byte đã gửi
+                        byte_sent += len(data)
+                        #Tính toán phần trăm và hiển thị tiến độ
+                        progress = (byte_sent/total_size)*100
+                        print(f"\rUploading: {progress:.2f}% completed", end="")
                 client_socket.send(b'END')
                 ack=client_socket.recv(1024)
+                print("\nUpload completed!")
 
         client_socket.send(b'FOLDER_END')
         client_socket.recv(1024)
@@ -51,20 +68,30 @@ def upload_file(client_socket, filepath):
 def download_file(client_socket, filename):
     client_socket.send(f"download {filename}".encode())
     response = client_socket.recv(1024).decode()
-    download_path = input("Nhap duong dan:  ")
-    if not os.path.isdir(download_path):
-        os.makedirs(download_path)
-    file_path = os.path.join(download_path, filename)
-    if response == "READY":
+   
+    if response.startswith("READY"):
+        #Lấy kích thước file do bên server gửi về 
+        _, file_size = response.split(" ")
+        file_size = int(file_size)
+        download_path = input("Nhap duong dan:  ")
+        if not os.path.isdir(download_path):
+            os.makedirs(download_path)
+        filename = os.path.basename(filename)
+        file_path = os.path.join(download_path, filename)
+
+        byte_received = 0
         with open(file_path, 'wb') as f:
             while True:
                 data = client_socket.recv(1024)
-                end_marker_index = data.find(b"END")
-                if end_marker_index + 3 == len(data):
-                    f.write(data[:end_marker_index])
+                if data == b"END":
                     break
                 else:
                     f.write(data)
+                    client_socket.send(b"ACK")
+                    byte_received += len(data)
+                    progress = (byte_received/file_size)*100
+                    print(f"\rDownloading {filename}: {progress:.2f}% completed", end="")
+
         print(f"File {filename} downloaded successfully")
     else:
         print(response)
